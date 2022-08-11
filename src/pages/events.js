ae82549029549faca9a133e361b865cb66cc4023
Liz <lee.anewed@gmail.com>
2022-08-11 10:21:54 -0500

    fix events page styling

--- src/pages/events.js
+++ src/pages/events.js
@@ -27,11 +27,18 @@ export default class Events extends React.PureComponent {
       <Article colorIndex="neutral-3">
         <DeltaHeader />
         <Container className="event-page" textAlign="center">
-          <div className="event-box">
-            <Container>
-              <Container textAlign="center">
-                <h1 className="event-head">Events</h1>
-              </Container>
+          <Container
+            id="event-header-box"
+            textAlign="center"
+            animation="fadeIn"
+          >
+            <h1 className="event-head">Events</h1>
+          </Container>
+          <div>
+            <Container className="event-box">
+              <h2 className="subheader" id="event-subheader">
+                Get Involved!
+              </h2>
               <Container text className="event-text">
                 <p id="event-description">
                   Celebrate, support, and participate in Baton Rouge and LSU's
@@ -60,51 +67,58 @@ export default class Events extends React.PureComponent {
                   <Icon size="huge" name="twitter square" />
                 </a>
               </Container>
-              <Container as="div" className="event-card">
-                <Image
-                  size="small"
-                  src={readingPic}
-                  // floated='left'
-                  circular={true}
-                  href="../highland-coffee-readings"
-                />
-                <a
-                  style={{ color: "#EEF2F6" }}
-                  href="../highland-coffee-readings"
-                  className="event-card-link"
-                >
-                  <h3 className="head-text">Highland Coffees Reading Series</h3>
-                </a>
-                <Container className="event-card-text">
-                  <p>
-                    An open mic for poetic souls and storytellers. Twice a
-                    month.
-                  </p>
+              <Box direction="row">
+                <Container as="div" className="event-card">
+                  <Image
+                    size="small"
+                    src={readingPic}
+                    // floated='left'
+                    circular={true}
+                    href="../highland-coffee-readings"
+                  />
+                  <a
+                    style={{ color: "#EEF2F6" }}
+                    href="../highland-coffee-readings"
+                    className="event-card-link"
+                  >
+                    <h3 className="head-text">
+                      Highland Coffees Reading Series
+                    </h3>
+                  </a>
+                  <Container className="event-card-text">
+                    <p>
+                      An open mic for poetic souls and storytellers. Twice a
+                      month.
+                    </p>
+                  </Container>
                 </Container>
-              </Container>
-              <Container className="event-card">
-                <Image
-                  //size="tiny"
-                  size="small"
-                  src={cloudGradient}
-                  // floated='left'
-                  circular={true}
-                  href="#"
-                />
-                <a
-                  style={{ color: "#EEF2F6" }}
-                  href="../highland-coffee-readings"
-                  // className='underline-a'
-                >
-                  <h3 className="head-text"> Legend-LARPing Costume Party </h3>
-                </a>
-                <Container className="event-card-text">
-                  <p>
-                    Come dressed as your favorite literary icon. Details coming
-                    soon!
-                  </p>
+                <Container className="event-card">
+                  <Image
+                    //size="tiny"
+                    size="small"
+                    src={cloudGradient}
+                    // floated='left'
+                    circular={true}
+                    href="#"
+                  />
+                  <a
+                    style={{ color: "#EEF2F6" }}
+                    href="../highland-coffee-readings"
+                    // className='underline-a'
+                  >
+                    <h3 className="head-text">
+                      {" "}
+                      Legend-LARPing Costume Party{" "}
+                    </h3>
+                  </a>
+                  <Container className="event-card-text">
+                    <p>
+                      Come dressed as your favorite literary icon. Details
+                      coming soon!
+                    </p>
+                  </Container>
                 </Container>
-              </Container>
+              </Box>
             </Container>
           </div>
         </Container>
